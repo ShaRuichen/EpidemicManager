286b3ee46341eb0c17d361f4938c3f6d2b70f0b2
ShaRuichen <1747308644@qq.com>
2020-08-15 11:44:34 +0800

    初步实现部分功能。

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -1,9 +1,44 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System;
+using System.Data;
+
+using Microsoft.AspNetCore.Mvc;
 
 namespace EpidemicManager.Controllers
 {
     public class LoginController : Controller
     {
+        private readonly Sql sql = new Sql();
 
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public JsonResult Login(string id, string password, string kind)
+        {
+            var person = kind switch
+            {
+                "people" => sql.Read("SELECT password FROM people WHERE id = @0", id),
+                "doctor" => sql.Read("SELECT password FROM doctor WHERE id = @0", id),
+                "manager" => sql.Read("SELECT password FROM manager WHERE id = @0", id),
+                _ => throw new InvalidOperationException()
+            };
+            if (person.Count == 0) return Json(new
+            {
+                isSucceeded = false,
+                message = "用户名不存在。",
+            });
+            var correctPassword = person[0][0].ToString();
+            if (password != correctPassword) return Json(new
+            {
+                isSucceeded = false,
+                message = "密码错误。",
+            });
+            return Json(new
+            {
+                isSucceeded = true,
+            });
+        }
     }
 }
--- wwwroot/js/login.js
+++ wwwroot/js/login.js
@@ -1,1 +1,17 @@
-﻿
+﻿function loginClick() {
+    var id = $('#userId').val()
+    var password = $('#userPassword').val()
+    var kind = $('input[name="userKind"]:checked').val()
+
+    $.post('/login/Login', { id: id, password: password, kind: kind }, function (result) {
+        var isSucceeded = result.isSucceeded
+        if (isSucceeded) {
+
+        }
+        else {
+            var message = result.message
+            $('#errorMessage').html(message)
+
+        }
+    })
+}
