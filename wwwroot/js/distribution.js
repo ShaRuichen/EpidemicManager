0758665741d4adaf4d8fcd9ac9596ce582db4d2b
czb1 <51730183+czb1@users.noreply.github.com>
2020-08-16 17:22:38 +0800

    Update distribution.js

--- wwwroot/js/distribution.js
+++ wwwroot/js/distribution.js
@@ -1,1 +1,20 @@
-﻿
+﻿function click1(str) {
+    $.post('/Distribution/Click', { name: str, number: 100 }, function (result) {
+        $('#button').html(result.name + result.num)
+    })
+}
+
+function click2(str1,str2) {
+    $.post('/Distribution/ClickToDistributeMoney', { hospname: str2, donid: str1 }, function (result) {
+        
+        $('#' + str1).remove();
+    })
+}
+
+
+function click3(str1, str2) {
+    $.post('/Distribution/ClickToDistributeMaterial', { hospname: str2, donid: str1 }, function (result) {
+       
+        $('#' + str1).remove();
+    })
+}
