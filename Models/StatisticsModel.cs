1ff6f69b5d72361adeb1d651eb1250d3dc242c44
Ray1852519 <1852519@tongji.edu.cn>
2020-08-17 11:28:59 +0800

    Create Statistics

---
+++ Models/StatisticsModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EpidemicManager.Models
+{
+    public class StatisticsModel//行程数据
+    {
+        public string ID { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public string Site { get; set; }
+    }
+}
