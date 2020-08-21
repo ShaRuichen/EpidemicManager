5aa662c777738863efb830a5b1252ab10eda776e
leyingzhang <57288034+leyingzhang@users.noreply.github.com>
2020-08-21 11:37:51 +0800

    更新检查报告属性

--- Models/ExamineModel.cs
+++ Models/ExamineModel.cs
@@ -1,4 +1,9 @@
+using System;
+using Microsoft.VisualBasic.CompilerServices;
 using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
 
 namespace EpidemicManager.Models
 {
@@ -8,14 +13,20 @@ namespace EpidemicManager.Models
         public string ID_patient { get; set; }
         public string ID_report { get; set; }
         public string time { get; set; }
+        public string date { get; set; }
         public string detail { get; set; }
         public string name_doctor { get; set; }
         public string name_patient { get; set; }
+        public string title { get; set; }
     }
     public class ExamineIndexModel//一个病人所有报告
     {
-        public string ID_patient { get; set; }
-        public string name_patient { get; set; }
-        public List<string> report { get; set; }
+        public List<string> ID_patient { get; set; }
+        public List<string> name_patient { get; set; }
+        public List<int> report { get; set; }
+        public List<string> title { get; set; }
+        public int n = 0;
     }
+
+
 }
