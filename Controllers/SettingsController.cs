1b48ab1873de57e8c820919010a1b7dea21d842c
HoldTheDoors <wsq15188516966@163.com>
2020-08-22 11:54:56 +0800

    fifth commit

--- Controllers/SettingsController.cs
+++ Controllers/SettingsController.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Data;
 using System;
 using EpidemicManager.Models;
+using Microsoft.AspNetCore.Http;
+
 
 namespace EpidemicManager.Controllers
 {
@@ -15,36 +17,64 @@ namespace EpidemicManager.Controllers
         [HttpGet]
         public IActionResult Changepeople()
         {
-            return View();
+            var id = HttpContext.Session.GetString("userId");
+            var kind = HttpContext.Session.GetString("userKind");
+            if (HttpContext.Session.GetString("userId") == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else if(HttpContext.Session.GetString("userKind") != null)
+            {
+                if (kind == "doctor") return RedirectToAction("Doctor");
+                else if (kind == "manager") return RedirectToAction("Manager");
+                else if (kind == "patient") return RedirectToAction("Patient");
+                else return View();
+            }
+            else
+            {
+                return View();
+            }
         }
         [HttpPost]
         public IActionResult Changepeople(SettingsPeople settingsPeople)
         {
+            var id = HttpContext.Session.GetString("userId");
             SettingsPeople people_info = new SettingsPeople();
-            people_info.people_id = Request.Form["settings_people_id"];
             people_info.name = Request.Form["name"];
             people_info.address = Request.Form["address"];
             people_info.tel = Request.Form["tel"];
             people_info.sex = Request.Form["sex"];
             people_info.password = Request.Form["password"];
-            Sql.Execute("UPDATE people set name=@1,address=@2,tel=@3,sex=@4 where id=@0", people_info.people_id, people_info.name, people_info.address, people_info.tel, people_info.sex);
+            Sql.Execute("UPDATE people set name=@1,address=@2,tel=@3,sex=@4 where id=@0", id, people_info.name, people_info.address, people_info.tel, people_info.sex);
             return RedirectToAction("Index");
         }
 
         [HttpGet]
         public IActionResult Doctor()
         {
-            return View();
+            var id = HttpContext.Session.GetString("userId");
+            if (HttpContext.Session.GetString("userId") == null)
+            {
+                return RedirectToAction("Index","Login");
+            }
+            else
+            {
+                var model = new Settingsdoctor
+                {
+                    doc_id = id,
+                };
+                return View(model);
+            }
         }
         [HttpPost]
         public IActionResult Doctor(Settingsdoctor settingsdoctor)
         {
             Settingsdoctor doc_info = new Settingsdoctor();
-            doc_info.doc_id = Request.Form["doc_id"];
+            var id = HttpContext.Session.GetString("userId");
             doc_info.hos_name = Request.Form["hos_name"];
             doc_info.name = Request.Form["name"];
             doc_info.password = Request.Form["password"];
-            Sql.Execute("UPDATE doctor set name=@1,hos_name=@2,password=@3 where id=@0", doc_info.doc_id, doc_info.name, doc_info.hos_name, doc_info.password);
+            Sql.Execute("UPDATE doctor set name=@1,hos_name=@2,password=@3 where id=@0", id, doc_info.name, doc_info.hos_name, doc_info.password);
             return RedirectToAction("Index");
         }
         [HttpGet]
@@ -56,13 +86,13 @@ namespace EpidemicManager.Controllers
         public IActionResult Manager(Settingsmanager settingsmanager)
         {
             Settingsmanager man_info = new Settingsmanager();
-            man_info.man_id = Request.Form["man_id"];
+            var id = HttpContext.Session.GetString("userId");
             man_info.name = Request.Form["name"];
             man_info.sex = Request.Form["sex"];
             man_info.tel = Request.Form["tel"];
             man_info.work_unit = Request.Form["work_unit"];
             man_info.password = Request.Form["password"];
-            Sql.Execute("UPDATE manager set name=@1,sex=@2,tel=@3,password=@4 where id=@0", man_info.man_id, man_info.name, man_info.sex, man_info.tel, man_info.password);
+            Sql.Execute("UPDATE manager set name=@1,sex=@2,tel=@3,password=@4 where id=@0", id, man_info.name, man_info.sex, man_info.tel, man_info.password);
             return RedirectToAction("Index");
         }
         [HttpGet]
@@ -74,12 +104,12 @@ namespace EpidemicManager.Controllers
         public IActionResult Patient(Settingspatient settingspatient)
         {
             Settingspatient pat_info = new Settingspatient();
-            pat_info.pat_id = Request.Form["pat_id"];
+            var id = HttpContext.Session.GetString("userId");
             pat_info.name = Request.Form["name"];
             pat_info.sex = Request.Form["sex"];
             pat_info.hos_name = Request.Form["hos_name"];
             pat_info.password = Request.Form["password"];
-            Sql.Execute("UPDATE patient set name=@1,hos_name=@2,sex=@3,password=@4 where id=@0", pat_info.pat_id, pat_info.name, pat_info.hos_name, pat_info.sex, pat_info.password);
+            Sql.Execute("UPDATE patient set name=@1,hos_name=@2,sex=@3,password=@4 where id=@0", id, pat_info.name, pat_info.hos_name, pat_info.sex, pat_info.password);
             return RedirectToAction("Index");
         }
         [HttpGet]
@@ -90,8 +120,8 @@ namespace EpidemicManager.Controllers
         [HttpPost]
         public IActionResult Delete(Settingspatient settingspatient)
         {
-            string person_id = Request.Form["person_id"];
-            string kind = Request.Form["kind"];
+            var person_id = HttpContext.Session.GetString("userId");
+            var kind = HttpContext.Session.GetString("userKind");
             if (kind == "doctor") Sql.Execute("DELETE from doctor where ID = @0", person_id);
             else if (kind == "patient") Sql.Execute("DELETE from patient where ID = @0", person_id);
             else if (kind == "manager") Sql.Execute("DELETE from manager where ID = @0", person_id);
