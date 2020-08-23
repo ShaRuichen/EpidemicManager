6438f2dff3d59caf9c569d90f00f5c2179f7b2ca
Teafox-Yang <69046815+Teafox-Yang@users.noreply.github.com>
2020-08-23 19:55:40 +0800

    Update questionnaire.js

--- wwwroot/js/questionnaire.js
+++ wwwroot/js/questionnaire.js
@@ -1,4 +1,4 @@
-﻿function addRadios() {
+function addRadios() {
     var parameters = {
         content: $('#radioContent').val(),
         op1: $('#option1').val(),
@@ -8,10 +8,10 @@
     };
     $.post('/Questionnaire/AddRadios', parameters, function (result) {
         if (result) {
-            $(location).attr('href', '/questionnaire/read')
+            $(location).attr('href', '/questionnaire/create')
         }
         else {
-            $('#radioErrorMessage').html('您没有设置题目，题目至少应有一个选项')
+            $('#radioErrorMessage').html('您没有设置好题目，题目应至少应有一个选项')
         }
     })
 }
@@ -22,25 +22,25 @@ function addFill() {
     };
     $.post('/Questionnaire/AddFill', parameters, function (result) {
         if (result) {
-            $(location).attr('href', '/questionnaire/read')
+            $(location).attr('href', '/questionnaire/create')
         }
         else {
             $('#fillErrorMessage').html('您没有设置题目')
         }
-    }
+    })
 }
 
 function deleteQuestion() {
     var parameters = {
-        delete_id: $('#deleteID')
-    }
+        delete_id: $('#deleteId').val(),
+    };
 
     $.post('/Questionnaire/DeleteQuestion', parameters, function (result) {
         if (result) {
-            $(location).attr('href', '/questionnaire/read')
+            $(location).attr('href', '/questionnaire/create')
         }
         else {
-            $('#managerErrorMessage').html('请输入正确的题号')
+            $('#deleteErrorMessage').html('请输入正确的题号')
         }
     })
 }
