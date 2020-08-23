fda3cb2ca2586d98ee3c8682ad96df3c4387d295
Ray1852519 <1852519@tongji.edu.cn>
2020-08-23 10:52:16 +0800

    .

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -1,9 +1,19 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using System.Collections.Generic;
+using System.Data;
+
+using Microsoft.AspNetCore.Mvc;
+using EpidemicManager.Models;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.AspNetCore.Http;
 
 namespace EpidemicManager.Controllers
 {
     public class StatisticsController : Controller
     {
+        public IActionResult Index()
+        {
 
+        }
     }
 }
--- Models/StatisticsModel.cs
+++ Models/StatisticsModel.cs
@@ -5,11 +5,16 @@ using System.Threading.Tasks;
 
 namespace EpidemicManager.Models
 {
-    public class StatisticsModel//行程数据
+    public class StatisticsModel//民众行程数据
     {
         public string ID { get; set; }
         public string Date { get; set; }
         public string Time { get; set; }
         public string Site { get; set; }
+    }
+
+    public class PepQRcodeModel//二维码
+    {
+        public string PepID { get; set; }
     }
 }
