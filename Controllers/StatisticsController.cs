23905af08de391625485089515af0b4c4dd8d60c
Ray1852519 <1852519@tongji.edu.cn>
2020-08-24 10:39:39 +0800

    添加项目文件

--- Controllers/StatisticsController.cs
+++ Controllers/StatisticsController.cs
@@ -6,11 +6,104 @@ using EpidemicManager.Models;
 using System.Diagnostics;
 using System.Threading;
 using Microsoft.AspNetCore.Http;
+using System;
 
 namespace EpidemicManager.Controllers
 {
     public class StatisticsController : Controller
     {
-        
+        public IActionResult Index()
+        {
+            var session = HttpContext.Session;
+            var userKind = session.GetString("userKind");
+            var userId = session.GetString("userId");
+            if(userId==null)
+            {
+                return RedirectPermanent("/login?path=statistics");
+            }
+
+            return View();
+        }
+
+        public IActionResult Read()
+        {
+            if(id!=null)
+            {
+                Sql.Execute("INSERT INTO people VALUES(@0, 'name', 'address', 'tel', 'sex', 'password')", id);
+            }
+            var ID = Sql.Read("SELECT ID FROM travel_info");
+            var Date = Sql.Read("SELECT date FROM travel_info");
+            var Time = Sql.Read("SELECT time FROM travel_info");
+            var Site = Sql.Read("SELECT site FROM travel_info");
+
+            foreach(DataRow travel in ID)
+            {
+                IDlist.Add(travel[0].ToString);
+            }
+            foreach (DataRow travel in date)
+            {
+                datelist.Add(travel[0].ToString);
+            }
+            foreach (DataRow travel in time)
+            {
+                timelist.Add(travel[0].ToString);
+            }
+            foreach (DataRow travel in site)
+            {
+                sitelist.Add(travel[0].ToString);
+            }
+
+            var model = new StatisticsModel
+            {
+                ID = IDlist,
+                Date = datelist,
+                Time = timelist,
+                Site = sitelist
+            };
+
+            return View(model);
+        }
+
+        public IActionResult PeopleAdd()
+        {
+            var session = HttpContext.Session;
+            var userKind = session.GetString("userKind");
+            var userId = session.GetString("userId");
+            var model = new PepQRcodeModel
+            {
+                PepID = userId
+            };
+
+            return View(model);
+        }
+
+        public IActionResult PeopleAdd_info()
+        {
+            var session = HttpContext.Session;
+            var userKind = session.GetString("userKind");
+            var userId = session.GetString("userId");
+
+            Models.PepModel PeopleInfo = new PepModel();
+            PeopleInfo.ID = Request.Form["MID"];
+            PeopleInfo.Site = Request.Form["Msite"];
+            string date = DateTime.Now.ToString("yyyy-MM-dd");
+            string time = DateTime.Now.ToString("T");
+            Sql.Execute("INSERT INTO travel_info VALUES(@0, @1, @2, @3)", PeopleInfo.ID, date, time, PeopleInfo.Site);
+
+            return View(PeopleInfo);
+        }
+
+        public IActionResult ShowQRcode()
+        {
+            var session = HttpContext.Session;
+            var userKind = session.GetString("userKind");
+            var userId = session.GetString("userId");
+            var model = new PepQRcodeModel
+            {
+                PepID = userId
+            };
+
+            return View(model);
+        }
     }
 }
--- Models/StatisticsModel.cs
+++ Models/StatisticsModel.cs
@@ -7,14 +7,22 @@ namespace EpidemicManager.Models
 {
     public class StatisticsModel//民众行程数据
     {
-        public string ID { get; set; }
-        public string Date { get; set; }
-        public string Time { get; set; }
-        public string Site { get; set; }
+        public List<string> ID { get; set; }
+        public List<string> Date{ get; set; }
+        public List<string> Time { get; set; }
+        public List<string> Site { get; set; }
+        public int Info_num { get; set; }
     }
 
     public class PepQRcodeModel//二维码
     {
         public string PepID { get; set; }
+        public string PepSite { get; set; }
+    }
+
+    public class PepModel
+    {
+        public string ID { get; set; }
+        public string Site { get; set; }
     }
 }
