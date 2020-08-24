765f99fd45bb0118d48011ec3acaa0706454ae36
achair8 <62297842+achair8@users.noreply.github.com>
2020-08-24 23:05:39 +0800

    Add files via upload

--- wwwroot/js/donation.js
+++ wwwroot/js/donation.js
@@ -1,1 +1,8 @@
-﻿
+﻿$(document).ready(function () {
+    if ($('#kind').attr("kind") != "people")
+    {
+        alert("只有民众可以进行捐献！");
+        window.location.href = "../Login/Index";
+    }
+}
+)
