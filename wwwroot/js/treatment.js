4dc8ae75dc6160205936b60a0ccc7879b351e3f5
ydmy760 <63996173+ydmy760@users.noreply.github.com>
2020-08-25 21:20:27 +0800

    Update treatment.js
    郝俊植

--- wwwroot/js/treatment.js
+++ wwwroot/js/treatment.js
@@ -21,8 +21,9 @@ function modifi() {
         if (result == 2) {
             var board = document.getElementById("staff");
             var store = new Array(2);
-            for (var i = 0; i < 2; i++) {
+            for (var i = 0; i < 4; i++) {
                 var e = document.createElement("input");
+
                 e.type = "text"
                 e.setAttribute("id", "input_" + i);//设置name属性
                 store[i] = e;
@@ -32,10 +33,15 @@ function modifi() {
             mmm.type = "button";
             mmm.setAttribute("value", "确定");
             mmm.setAttribute("onclick", "insert()");
+            mmm.setAttribute("id","cc");
             board.appendChild(mmm);
+            store[0].setAttribute("type", "date");
+            store[1].setAttribute("type", "time");
             $.post('/Treatment/Give', { plan_id2: document.getElementById("plan_id2").value }, function (result) {
-                store[0].setAttribute("value", result[0]);
+                store[0].setAttribute("value", result[0].substr(0, 10));
                 store[1].setAttribute("value", result[1]);
+                store[2].setAttribute("value", result[2]);
+                store[3].setAttribute("value", result[3]);
             })
         }
         //$('#button').html(result.plan_id + result.detail)
@@ -44,7 +50,22 @@ function modifi() {
 
 function insert() {
     $.post('/Treatment/Update',
-        { plan_id: document.getElementById("plan_id2").value, med: document.getElementById("input_0").value, det: document.getElementById("input_1").value }, function (result) {
-        if (result == 1) alert("修改成功");
+        { plan_id: document.getElementById("plan_id2").value, date: document.getElementById("input_0").value, time: document.getElementById("input_1").value, med: document.getElementById("input_2").value, det: document.getElementById("input_3").value }, function (result) {
+            alert("修改成功");
+            window.location.href = "../Treatment/Check";
+    })
+    
+}
+
+function CheckLogin1() {
+    $.post('/Treatment/CheckLogin', function (result) {
+        if (result == 0) { alert("请您先登录"); window.location.href = "../Login?path=../Treatment/Check"; }
+        if (result == 1) { alert("当前不是医生请登陆"); window.location.href = "../Login?path=../Treatment/Check";}
+    })
+}
+function CheckLogin2() {
+    $.post('/Treatment/CheckLogin', function (result) {
+        if (result == 0) { alert("请您先登录"); window.location.href = "../Login?path=../Treatment/Insert"; }
+        if (result == 1) { alert("当前不是医生请登陆"); window.location.href = "../Login?path=../Treatment/Insert"; }
     })
 }
