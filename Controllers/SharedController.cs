5ab8285dd6acd32b73276e909459bb6c46b6f868
ShaRuichen <1747308644@qq.com>
2020-09-07 16:51:08 +0800

    初步实现民众导航。

--- Controllers/SharedController.cs
+++ Controllers/SharedController.cs
@@ -1,9 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace EpidemicManager.Controllers
 {
     public class SharedController : Controller
     {
-
+        [HttpGet]
+        public IActionResult Logout()
+        {
+            var session = HttpContext.Session;
+            session.Remove("userId");
+            session.Remove("userKind");
+            return RedirectToAction("Index", "Home");
+        }
     }
 }
