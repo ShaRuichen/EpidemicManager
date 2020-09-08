161074594628cd1ba2e23c02a2e4f375ff16fbb9
ShaRuichen <1747308644@qq.com>
2020-09-08 09:39:06 +0800

    优化导航栏。

--- wwwroot/js/shared.js
+++ wwwroot/js/shared.js
@@ -1,3 +1,17 @@
 ﻿function s_jump(target) {
     $(location).attr('href', target)
 }
+
+var active
+
+$(document).ready(function () {
+    $("#navigation").find("li").each(function () {
+        var a = $(this).find("a:first")[0];
+        if ($(a).attr("href") === location.pathname) {
+            $(this).addClass("active");
+        } else {
+            $(this).removeClass("active");
+        }
+    });
+});
+
