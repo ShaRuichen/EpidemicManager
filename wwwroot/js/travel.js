75ce8fe50a46f7103f73f761ccb11ee62c115ec9
Rhysdid <49804943+Rhysdid@users.noreply.github.com>
2020-09-17 16:38:52 +0800

    Update travel.js

--- wwwroot/js/travel.js
+++ wwwroot/js/travel.js
@@ -1,5 +1,22 @@
-﻿function click1(str) {
-    $.post('/Template/Click', { name: str, number: 100 }, function (result) {
-        $('#button').html(result.name + result.num)
-    })
+function getLocation() {
+    var geolocation = new BMap.Geolocation();
+    var gc = new BMap.Geocoder();
+    geolocation.getCurrentPosition(function (r) {
+        console.log(r);
+        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+            var pt = r.point;
+            gc.getLocation(pt, function (rs) {
+                var addComp = rs.addressComponents;
+                var province = addComp.province;
+                var city = addComp.city;
+                var area = addComp.district;
+                document.getElementById('site1').value = province + ":" + city + ":" + area;
+                $("#site").text(province + ":" + city + ":" + area);
+                $("#button").text(province + ":" + city + ":" + area);
+            });
+        }
+        else {
+            alert("定位失败");
+        }
+    }, { enableHighAccuracy: true });
 }
