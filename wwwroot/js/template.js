aee9f2a9b155586772e260513747cbe9ae478e4b
ShaRuichen <1747308644@qq.com>
2020-09-17 08:49:50 +0800

    实现定位

--- wwwroot/js/template.js
+++ wwwroot/js/template.js
@@ -3,3 +3,24 @@
         $('#button').html(result.name + result.num)
     })
 }
+
+function getLocation() {
+    var geolocation = new BMap.Geolocation();
+    var gc = new BMap.Geocoder();
+    geolocation.getCurrentPosition(function (r) {
+        console.log(r);
+        if (this.getStatus() == BMAP_STATUS_SUCCESS) {
+            var pt = r.point;
+            gc.getLocation(pt, function (rs) {
+                var addComp = rs.addressComponents;
+                var province = addComp.province;
+                var city = addComp.city;
+                var area = addComp.district;
+                alert(province + ":" + city + ":" + area);
+            });
+        }
+        else {
+            alert("定位失败");
+        }
+    }, { enableHighAccuracy: true });
+}
