c6a3f46e3f713d6038e097faeb7d9555a6b824da
ShaRuichen <1747308644@qq.com>
2020-09-18 15:25:24 +0800

    .

--- wwwroot/js/examine.js
+++ wwwroot/js/examine.js
@@ -1,18 +1,18 @@
 function create_() {
     $.post('/Examine/Create_', { p_id: document.getElementById("patient_id").value, title: document.getElementById("title").value, detail: document.getElementById("detail").value  }, function (result) {
         if (result == 1) {
-            alert("创建成功");
+            alert("鍒涘缓鎴愬姛");
             //$('#').remove();
             window.location.href = "../Examine/Index_doctor";
         }
         
         else if(result==0) {
-            //alert("病人身份证号输入有误");
+            //alert("鐥呬汉韬唤璇佸彿杈撳叆鏈夎");
            // window.location.href = "../Examine/Create";
             $('.popover-dismiss').popover({
                 trigger: 'focus'
             })
-            alert("病人身份证号输入有误");
+            alert("鐥呬汉韬唤璇佸彿杈撳叆鏈夎");
            window.location.href = "../Examine/Create";
         }
     })
@@ -21,7 +21,7 @@ function create_() {
 function write_() {
     $.post('/Examine/Write_', { r_id: document.getElementById("report_id").value,p_id: document.getElementById("patient_id").value, title: document.getElementById("title").value, detail: document.getElementById("detail").value }, function (result) {
         if (result == 1) {
-            alert("创建成功");
+            alert("鍒涘缓鎴愬姛");
             window.location.href = "../Examine/Write_index";
         }
 
