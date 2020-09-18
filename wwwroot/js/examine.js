2bcd5ea2061100049ffda6f4bda725cfd0e264c3
leyingzhang <57288034+leyingzhang@users.noreply.github.com>
2020-09-18 15:53:19 +0800

    限制页面更新

--- wwwroot/js/examine.js
+++ wwwroot/js/examine.js
@@ -1,18 +1,18 @@
 function create_() {
     $.post('/Examine/Create_', { p_id: document.getElementById("patient_id").value, title: document.getElementById("title").value, detail: document.getElementById("detail").value  }, function (result) {
         if (result == 1) {
-            alert("鍒涘缓鎴愬姛");
+            alert("创建成功");
             //$('#').remove();
             window.location.href = "../Examine/Index_doctor";
         }
         
         else if(result==0) {
-            //alert("鐥呬汉韬唤璇佸彿杈撳叆鏈夎");
+            //alert("病人身份证号输入有误");
            // window.location.href = "../Examine/Create";
             $('.popover-dismiss').popover({
                 trigger: 'focus'
             })
-            alert("鐥呬汉韬唤璇佸彿杈撳叆鏈夎");
+            alert("病人身份证号输入有误");
            window.location.href = "../Examine/Create";
         }
     })
@@ -21,10 +21,13 @@ function create_() {
 function write_() {
     $.post('/Examine/Write_', { r_id: document.getElementById("report_id").value,p_id: document.getElementById("patient_id").value, title: document.getElementById("title").value, detail: document.getElementById("detail").value }, function (result) {
         if (result == 1) {
-            alert("鍒涘缓鎴愬姛");
+            alert("创建成功");
             window.location.href = "../Examine/Write_index";
         }
-
+        else {
+            alert("病人ID输入有误");
+        }
     })
+    return false;
 }
 
